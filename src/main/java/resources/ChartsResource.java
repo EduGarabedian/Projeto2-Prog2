6c647dec7c8bd799ca8b995dbd4584d7736c276a
lucarmo-dev <carmo.lucas.lc@gmail.com>
2020-06-21 17:28:30 -0300

    ChartsResources

--- src/main/java/resources/ChartsResource.java
+++ src/main/java/resources/ChartsResource.java
@@ -1,4 +1,23 @@
 package resources;
+import java.util.ArrayList;
+import java.util.List;
+
+
 
 public class ChartsResource {
-}
+
+
+
+
+    public class ChartsResource {
+        ChartsDAO dao;
+
+        public ChartsResource(ChartsDAO dao){
+            this.dao=dao;
+        }
+
+
+        public List<Results>getResults(){
+            return this.dao.read();
+        }
+
