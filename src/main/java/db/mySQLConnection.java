8e1275d261b97de1045c3a9d5de298a54c2dcca3
Eduardo <edugarabedian@hotmail.com>
2020-06-21 16:57:09 -0300

    MySQL Connection criado

--- src/main/java/db/mySQLConnection.java
+++ src/main/java/db/mySQLConnection.java
@@ -1,4 +1,20 @@
 package db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class mySQLConnection {
+    String url="jdbc:mysql://127.0.0.1:3306/mack_edu";
+    String usuario="Edu";
+    String senha="knightartorias";
+
+    public Connection getConnection(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(url,usuario,senha);
+        }catch (final Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
