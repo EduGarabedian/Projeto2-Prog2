0dfe222be6175ed9bd4e5d2c33625079d1af9220
Eduardo <edugarabedian@hotmail.com>
2020-06-21 18:17:04 -0300

    Main Update

--- src/main/java/resources/Main.java
+++ src/main/java/resources/Main.java
@@ -1,7 +1,59 @@
 package resources;
 
-public class Main {
-    public static void main(String[] args) {
+import org.db.ChartsDao;
+import io.dropwizard.Application;
+import io.dropwizard.Configuration;
+import io.dropwizard.assets.AssetsBundle;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import java.util.EnumSet;
+
+public class Main extends Application<Configuration>{
+    public static void main( String[] args ) {
+        try {
+            new Main().run(args);
+        }catch(Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+
+    @Override
+    public String getName() {
+        return "hello-world";
+    }
+
+    @Override
+    public void initialize(Bootstrap<Configuration> bootstrap) {
+        AssetsBundle assetsBundle = new AssetsBundle("/site", "/", "index.html");
+        bootstrap.addBundle(assetsBundle);
+    }
+
+    @Override
+    public void run(Configuration configuration, Environment environment) {
+
+        // Enable CORS headers
+        final FilterRegistration.Dynamic cors =
+                environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+
+        // Configure CORS parameters
+        cors.setInitParameter("allowedOrigins", "*");
+        cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin");
+        cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+
+        // Add URL mapping
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+
+        // Registrando Recursos
+        ChartsDao dao = new ChartsDao();
+        ChartsResource trendsResource = new ChartsResource(dao);
+        environment.jersey().register(trendsResource);
+
+        // Mudar rotas dos recursos
+        environment.jersey().setUrlPattern("/api/*");
     }
 }
