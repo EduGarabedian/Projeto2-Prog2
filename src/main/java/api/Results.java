9cf7e4ae14659651105052a11d9dc629f6f50485
Eduardo <edugarabedian@hotmail.com>
2020-06-21 17:25:38 -0300

    Update Results

--- src/main/java/api/Results.java
+++ src/main/java/api/Results.java
@@ -1,6 +1,24 @@
 package api;
 
 public class Results {
+    private String date;
+    private double value;
+
+    public String getDate(){
+        return date;
+    }
+
+    public void setDate(String date){
+        this.date=date;
+    }
+
+    public double getValue(){
+        return value;
+    }
+
+    public void setValue(double value){
+        this.value=value;
+    }
 
 
 }
