2f097f6c69b41a5d084e753aa94e3e367d9f1e57
lucarmo-dev <carmo.lucas.lc@gmail.com>
2020-06-21 16:56:10 -0300

    ChartsDao
    Configuaracao da classe

--- src/main/java/db/ChartsDao.java
+++ src/main/java/db/ChartsDao.java
@@ -1,4 +1,18 @@
 package db;
+import api.Charts;
 
-public class ChartsDao {
+
+public abstract class ChartsDao {
+
+
+        abstract boolean create(Charts var1);
+
+        Lista<Charts> read();
+
+        boolean update(Charts var1);
+
+        boolean delete(Charts var1);
+
+
+
 }
